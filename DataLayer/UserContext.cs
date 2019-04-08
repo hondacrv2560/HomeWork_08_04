785b13598bf52191a1990362c726cc500855fbbf
Elias Kosogov <i.kosogov@gmail.com>
2019-04-08 14:12:04 +0300

    Add class to DataLayer UserContext

---
+++ DataLayer/UserContext.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+
+        public UserContext() : base("name=UserConnectionString")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
