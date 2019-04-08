cd349ec433588fab2f2dc8f99b6dce106d1f3773
Elias Kosogov <i.kosogov@gmail.com>
2019-04-08 14:08:35 +0300

    Add clas to DataModel User, Role, UserInRole

---
+++ DataLayer/DataModel/Role.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DataModel
+{
+    [Table("Role")]
+    public class Role
+    {
+        [Key]
+        [Column("RoleId")]
+        public int RoleId { get; set; }
+        [ForeignKey("RoleId")]
+        [Required]
+        [MaxLength(50)]
+        [Column("RoleName")]
+        public string RoleName { get; set; }
+        [Column("Description")]
+        public string Description { get; set; }
+
+        public virtual List<UserInRole> UserInRoles { get; set; }
+
+        public Role()
+        {
+            UserInRoles = new List<UserInRole>();
+        }
+
+    }
+}
---
+++ DataLayer/DataModel/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DataModel
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [Column("UserId")]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        [Required]
+        [MaxLength(50)]
+        [Column("Email")]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(50), MinLength(3)]
+        [Column("UserName")]
+        public string UserName { get; set; }
+        [Column("DateBirthday")]
+        public DateTime DateBirthday { get; set; }
+        [Column("JobTitleId")]
+        public int? JobTitleId { get; set; }
+
+        public virtual List<UserInRole> UserInRoles { get; set; }
+
+        public User()
+        {
+            UserInRoles = new List<UserInRole>();
+        }
+    }
+}
